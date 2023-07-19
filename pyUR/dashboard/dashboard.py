e895fe2c703f918ac901780fb5f9de39a1f12cd9
Hristo Ganchev <hristo.iliev.ganchev@gmail.com>
2023-07-19 22:32:13 +0300

    receive connected after connection to robot

--- pyUR/dashboard/dashboard.py
+++ pyUR/dashboard/dashboard.py
@@ -7,12 +7,21 @@ class dashboard:
     '''
     def __init__(self, host_ip):
         self.host = host_ip
-        self.port = 20000
+        self.port = 29999
         self._init_socket()
 
     def _init_socket(self):
         self.dashboard_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.dashboard_socket.connect((self.host, self.port))
+        connected = self.dashboard_socket.recv(1024).decode('utf-8')
+        if 'Connected' in connected:
+            self.connected = True
+            print('Dashboard connected')
+        else :
+            self.connected = False
+        
+    def is_connected(self) -> bool:
+        return self.connected
 
     '''
     function for send/receive to socket
@@ -20,6 +29,7 @@ class dashboard:
     '''
     def send_receive_socket(self, dashboard_command:str) -> str:
         try:
+            print(dashboard_command)
             self.dashboard_socket.send(dashboard_command.encode('utf-8'))
             return str(self.dashboard_socket.recv(1024).decode('utf-8'))
         except:
